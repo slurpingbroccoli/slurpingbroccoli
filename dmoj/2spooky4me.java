768c63947f68313cc8f7ed3499a69203b4b2e4fd
lily <lilyyou56@gmail.com>
2024-10-23 15:34:08 -0400

    Create 2spooky4me.java

---
+++ dmoj/2spooky4me.java
@@ -0,0 +1,10 @@
+import java.util.scanner
+
+public class 2spook4me {
+  public static void main(String[]args){
+    Scanner nums = scanner(System.in);
+    int N = nums.nextInt();
+    int L = nums.nextInt();
+    int S = nums.nextInt();
+    for(int i = 1; i < N; i++)
+      
